eabdedcfb29c0566be881d41369d4ed5b5c1decc
gokmen.yilmaz <gokmen1977@gmail.com>
2022-01-31 18:57:02 +0300

    'deploy1'

--- app1.js
+++ app1.js
@@ -10,7 +10,7 @@ const canvas=document.querySelector("canvas");
 
 btn.addEventListener("click", ()=>{
 
-  const constraint={video: {width:400, height:400, facingMode: 'environment'}}
+  const constraint={video: {width:300, height:300, facingMode: 'environment'}}
   navigator.mediaDevices.getUserMedia(constraint)
   .then(function(stream) {
    
@@ -18,19 +18,20 @@ btn.addEventListener("click", ()=>{
     video.play();
 
     const ctx=canvas.getContext('2d');
-    canvas.width=400;
-    canvas.height=400;
+    canvas.width=300;
+    canvas.height=300;
 
   
-
+   i=0;
     setInterval(() => {
-      ctx.drawImage(video,0,0,400,400);
+      ctx.drawImage(video,0,0,300,300);
 
       var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
 
+      i++;
       barcodeDetector.detect(canvas)
       .then(data => {
-        lblSonuc.innerHTML=data;
+        lblSonuc.innerHTML=i;
       })
       .catch(err => {
         alert("hata:",err.message);
