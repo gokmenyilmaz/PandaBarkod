3818dd2fb2b0905e6c1f87125489c12f9e617d40
gokmen.yilmaz <gokmen1977@gmail.com>
2022-01-31 18:32:26 +0300

    'deploy1'

--- root/app1.js
+++ root/app1.js
@@ -22,8 +22,8 @@ btn.addEventListener("click", ()=>{
     try {
       var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
       alert("barkod nesne oluştu");
-    } catch (error) {
-      alert(error.message);
+    } catch (err) {
+      alert("nesne oluşma hatası:" + err.message);
     }
   
 
@@ -42,7 +42,7 @@ btn.addEventListener("click", ()=>{
 
   })
   .catch(function(err) {
-    alert(err.message);
+    alert("strream alma hatası:" + err.message);
     
   });
 
