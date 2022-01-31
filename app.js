4697f3c56ce16d758c11b85f227429c560297449
gokmen.yilmaz <gokmen1977@gmail.com>
2022-01-31 17:30:37 +0300

    'deploy1'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var btn=document.getElementById("btnBarkod");
 var sonucLabel=document.getElementById("lblSonuc");
 const imageEl = document.querySelector("img");
 const video=document.querySelector("video");
+const canvas=document.querySelector("canvas");
 
 
 btn.addEventListener("click", ()=>{
@@ -15,6 +16,17 @@ btn.addEventListener("click", ()=>{
 
     video.onloadedmetadata = function(e) {
       video.play();
+
+      const ctx=canvas.getContext('2d');
+
+      canvas.width=video.width;
+      canvas.height=video.height;
+
+      setInterval(() => {
+        ctx.drawImage(video,0,0,ctx.width,ctx.height);
+        
+      }, 100);
+
     };
    
    
